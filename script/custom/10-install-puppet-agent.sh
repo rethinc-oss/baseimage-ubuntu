2fd4beb7ba8d80e3fdc96b76afed8ec9d3b6f1e3
Tim Kettler <tim.kettler@rethinc.de>
2019-03-27 14:11:11 +0100

    Add puppet bin directory to the sudo secure path.

--- script/custom/10-install-puppet-agent.sh
+++ script/custom/10-install-puppet-agent.sh
@@ -9,3 +9,7 @@ DEBIAN_FRONTEND=noninteractive dpkg -i puppet6-release-${CODENAME}.deb
 apt update
 apt install puppet-agent
 rm puppet6-release-${CODENAME}.deb
+
+# add puppet bin directory to the sudo secure path
+SECURE_PATH="Defaults secure_path=\"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/snap/bin:/opt/puppetlabs/bin\""
+echo $SECURE_PATH > /etc/sudoers.d/secure-path
