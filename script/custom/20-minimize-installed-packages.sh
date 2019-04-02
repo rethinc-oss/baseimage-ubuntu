15b2f3d66ef150156e18e2b5a005ae62c245b924
Tim Kettler <tim.kettler@rethinc.de>
2019-04-02 19:03:56 +0200

    Don't uninstall librm2; disable ubuntu-advantage cronjob.

--- script/custom/20-minimize-installed-packages.sh
+++ script/custom/20-minimize-installed-packages.sh
@@ -10,8 +10,9 @@ apt -y purge btrfs-tools cpp cpp-5 ed gcc gcc-5 krb5-locales language-pack-gnome
        plymouth-theme-ubuntu-text popularity-contest screen snapd ufw vim vim-common \
        vim-runtime vim-tiny fakeroot keyutils libalgorithm-diff-perl \
        libalgorithm-diff-xs-perl libalgorithm-merge-perl libasan2 libatomic1 \
-       libc-dev-bin libc6-dev libcc1-0 libcilkrts5 libdrm2 libfakeroot libgcc-5-dev \
+       libc-dev-bin libc6-dev libcc1-0 libcilkrts5 libfakeroot libgcc-5-dev \
        libgomp1 libisl15 libitm1 liblsan0 libmpc3 libmpx0 libnfsidmap2 libplymouth4 \
        libquadmath0 libstdc++-5-dev libtirpc1 libtsan0 libubsan0 linux-libc-dev \
        manpages-dev rpcbind
        
+rm /etc/cron.daily/ubuntu-advantage-tools
